438ec0c3791f94f2beb1a2f533142917e4634ba3
alejandro <grajalesalejandro068@gmail.com>
2025-01-27 12:11:10 +0100

    avances

---
+++ src/main/java/com/mycompany/reportenfl/ReporteNFL.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reportenfl;
+
+import java.awt.Menu;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author alejandro
+ */
+public class ReporteNFL {
+
+    public static void main(String[] args) {
+              
+                Reportnfl rep= new Reportnfl();
+              rep.setVisible(true);
+                
+    }
+}
